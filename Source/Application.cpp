fbc4e333d5a95b2a493138762f1c51d04f887f58
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2022-11-17 13:39:46 +0100

    low quality dpi scale.

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -37,6 +37,8 @@ namespace FLOOF {
         //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
         io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
         io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+        io.ConfigFlags |= ImGuiConfigFlags_DpiEnableScaleViewports;
+        io.ConfigFlags |= ImGuiConfigFlags_DpiEnableScaleFonts;
         //io.ConfigViewportsNoAutoMerge = true;
         //io.ConfigViewportsNoTaskBarIcon = true;
 
