d89514b787bc512d3ef928d7d4d2e5e5c2c90917
Adrevland <adrian.l.drevland@gmail.com>
2022-11-21 14:55:18 +0100

    new scene

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -79,7 +79,7 @@ namespace FLOOF {
             stbi_image_free(images[i].pixels);
         }
 
-        SelectDebugScene(DebugScenes::Physics);
+        SelectDebugScene(DebugScenes::PhysicsPlayground);
 
         m_ApplicationLayers.emplace_back(std::make_unique<EditorLayer>());
 
@@ -488,6 +488,74 @@ namespace FLOOF {
     }
 
     void Application::MakePhysicsPlayGround() {
+        m_Scene = std::make_unique<Scene>();
 
+        //make flooring
+        {
+            auto location = glm::vec3(0.f, -50.f, 0.f);
+            auto extents = glm::vec3(1000.f, 5.f, 1000.f);
+            auto mass = 0.f;
+
+            auto entity = m_Scene->CreateEntity("flooring");
+            auto &collision = m_Scene->AddComponent<RigidBodyComponent>(entity, location, extents, mass, bt::CollisionPrimitive::Box);
+            auto &mesh = m_Scene->AddComponent<StaticMeshComponent>(entity, "Assets/Primitives/IdentityCube.fbx");
+            mesh.meshes[0].MeshMaterial.Diffuse = Texture("Assets/crisscross-foam1-ue/crisscross-foam_albedo.png");
+            mesh.meshes[0].MeshMaterial.AO = Texture("Assets/crisscross-foam1-ue/crisscross-foam_ao.png");
+            mesh.meshes[0].MeshMaterial.Metallic = Texture("Assets/crisscross-foam1-ue/crisscross-foam_metallic.png");
+            mesh.meshes[0].MeshMaterial.Normals = Texture("Assets/crisscross-foam1-ue/crisscross-foam_normal-dx.png");
+            mesh.meshes[0].MeshMaterial.Roughness = Texture("Assets/crisscross-foam1-ue/crisscross-foam_roughness.png");
+            mesh.meshes[0].MeshMaterial.UpdateDescriptorSet();
+
+            auto &transform = m_Scene->GetComponent<TransformComponent>(entity);
+            transform.Position = glm::vec3(collision.Transform.getOrigin().getX(),
+                                           collision.Transform.getOrigin().getY(),
+                                           collision.Transform.getOrigin().getZ());
+            transform.Scale = extents;
+            collision.RigidBody->setFriction(1.0f);
+
+            //place random ramps
+            {
+                auto mass = 0.f;
+
+                //blanket textures;
+                const char *albedos[4]{
+                        "Assets/soft-blanket-ue/soft-blanket_Blue_albedo.png",
+                        "Assets/soft-blanket-ue/soft-blanket_Pink_albedo.png",
+                        "Assets/soft-blanket-ue/soft-blanket_Red_albedo.png",
+                        "Assets/soft-blanket-ue/soft-blanket_Yellow_albedo.png",
+                };
+
+                for (int i{0}; i < 10.f; i++) {
+                    auto extents = glm::vec3(Math::RandFloat(5.f, 30.f), Math::RandFloat(2.f, 10.f), Math::RandFloat(5.f, 30.f));
+                    auto location = glm::vec3(Math::RandFloat(-200.f, 200.f), -43.f + extents.y, Math::RandFloat(-200.f, 200.f));
+                    auto rotation = glm::vec3(0.f, Math::RandFloat(0.f, 6.28f), 0.f);
+                    std::string name = "Random ramp ";
+                    name += std::to_string(i);
+
+                    auto entity = m_Scene->CreateEntity(name);
+                    auto &collision = m_Scene->AddComponent<RigidBodyComponent>(entity, location, extents, rotation, mass, "Assets/Primitives/IdentityRamp.fbx");
+                    auto &mesh = m_Scene->AddComponent<StaticMeshComponent>(entity, "Assets/Primitives/IdentityRamp.fbx");
+                    auto texture = Math::RandInt(0, 3);
+                    mesh.meshes[0].MeshMaterial.Diffuse = Texture(albedos[texture]);
+                    mesh.meshes[0].MeshMaterial.AO = Texture("Assets/soft-blanket-ue/soft-blanket_ao.png");
+                    mesh.meshes[0].MeshMaterial.Metallic = Texture("Assets/soft-blanket-ue/soft-blanket_metallic.png");
+                    mesh.meshes[0].MeshMaterial.Normals = Texture("Assets/soft-blanket-ue/soft-blanket_normal-dx.png");
+                    mesh.meshes[0].MeshMaterial.Roughness = Texture("Assets/soft-blanket-ue/soft-blanket_roughness.png");
+
+                    mesh.meshes[0].MeshMaterial.UpdateDescriptorSet();
+
+                    auto &transform = m_Scene->GetComponent<TransformComponent>(entity);
+                    transform.Position = glm::vec3(collision.Transform.getOrigin().getX(), collision.Transform.getOrigin().getY(), collision.Transform.getOrigin().getZ());
+                    transform.Scale = extents;
+                    collision.RigidBody->setFriction(0.9f);
+                }
+            }
+
+        }
+        // make monstertruck
+        {
+            auto ent = m_Scene->CreateEntity("MonsterTruck");
+            m_Scene->AddComponent<NativeScriptComponent>(ent, std::make_unique<MonsterTruckScript>(), m_Scene, ent);
+        }
     }
 }
