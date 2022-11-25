aa1c249373943e1288a8d4a1ec87c1cb7f2f1d90
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2022-11-25 12:07:38 +0100

    Shadowpass progress.

--- Source/Renderer/Framebuffer.h
+++ Source/Renderer/Framebuffer.h
@@ -41,19 +41,18 @@ namespace FLOOF {
 		VulkanTexture GetTexture() { return m_Texture; }
 
 	private:
-		void Create();
 		void CreateFramebufferTexture();
-		void CreateFramebufferObject(VkImageView imageView);
+		void CreateFramebufferObject();
+		void CreateRenderPass();
 
-		void Destroy();
 		void DestroyFramebufferTexture();
 		void DestroyFramebufferObject();
-
-		void CreateRenderPass();
 		void DestroyRenderPass();
 
 		VulkanTexture m_Texture{};
 		VkFramebuffer m_Framebuffer = VK_NULL_HANDLE;
 		VkRenderPass m_RenderPass = VK_NULL_HANDLE;
+		VkFormat m_Format{};
+		VkExtent2D m_Extent{};
 	};
 }
