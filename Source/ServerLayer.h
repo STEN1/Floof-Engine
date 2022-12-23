573dd6264677f53833555cc9f8fdf663c870a637
Adrevland <adrian.l.drevland@gmail.com>
2022-12-23 22:04:17 +0100

    linux compile fix?

---
+++ Source/ServerLayer.h
@@ -0,0 +1,15 @@
+#ifndef FLOOF_SERVERLAYER_H
+#define FLOOF_SERVERLAYER_H
+
+#include "ApplicationLayer.h"
+
+namespace FLOOF{
+    class ServerLayer : public ApplicationLayer {
+
+
+    };
+}
+
+
+
+#endif //FLOOF_SERVERLAYER_H
