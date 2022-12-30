31ba41e6ec68eb5b6ed923863f23e0d25f86d2b2
Sten Rune Lyngmo <stenrunelyngmo@gmail.com>
2022-12-30 21:22:11 +0100

    removed balls.

--- Source/PointLightComponent.h
+++ Source/PointLightComponent.h
@@ -4,7 +4,7 @@
 namespace FLOOF {
     struct PointLightComponent {
         glm::vec4 diffuse = { 1.0f, 0.7f, 0.5f, 0.f };
-        float intensity = 50000.f;
+        float intensity = 500000.f;
         float innerRange = 32.f;
         float outerRange = 128.f;
 
