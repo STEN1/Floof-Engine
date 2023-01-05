9dea1cd07530779f2030c8111b032e548012ed2d
Espen Fodstad <esfod2001@gmail.com>
2023-01-05 13:05:18 +0100

    terrain smooth-er

--- Source/Editor/EditorLayer.cpp
+++ Source/Editor/EditorLayer.cpp
@@ -297,7 +297,7 @@ namespace FLOOF {
         //terrain
         {
             auto entity = m_Scene->CreateEntity("Terrain");
-            auto &mesh = m_Scene->AddComponent<LandscapeComponent>(entity, "Assets/Terrain/Terrain_Tough/heightMap.png", "Assets/Terrain/Terrain_Tough/texture.png");
+            auto &mesh = m_Scene->AddComponent<LandscapeComponent>(entity, "Assets/Terrain/Terrain_Tough/heightmap.png", "Assets/Terrain/Terrain_Tough/texture.png");
 
             auto &transform = m_Scene->GetComponent<TransformComponent>(entity);
             //transform.Position = glm::vec3(mesh.landscape->width/2.f,-100,mesh.landscape->height/2.f);
@@ -801,7 +801,7 @@ namespace FLOOF {
             std::string name = "Heightmap";
 
             auto entity = m_Scene->CreateEntity(name);
-            auto &mesh = m_Scene->AddComponent<LandscapeComponent>(entity, "Assets/Terrain/Terrain_Tough/heightMap.png", "Assets/Terrain/Terrain_Tough/texture.png");
+            auto& mesh = m_Scene->AddComponent<LandscapeComponent>(entity, "Assets/Terrain/Terrain_Tough/heightmap.png", "Assets/Terrain/Terrain_Tough/texture.png");
         }
     }
 
