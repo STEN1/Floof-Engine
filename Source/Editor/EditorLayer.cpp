5a4eee21e24a16b9d0009679811859d672e6af83
Adrevland <adrian.l.drevland@gmail.com>
2023-01-05 16:26:03 +0100

    ramps

--- Source/Editor/EditorLayer.cpp
+++ Source/Editor/EditorLayer.cpp
@@ -1008,7 +1008,7 @@ namespace FLOOF {
             auto *state = new btDefaultMotionState();
             btRigidBody::btRigidBodyConstructionInfo info(0, state, mesh.HeightFieldShape->mHeightfieldShape);
             auto *body = new btRigidBody(info);
-            body->setFriction(1.f);
+            body->setFriction(1.5f);
             m_Scene->GetPhysicSystem()->AddRigidBody(body);
         }
 
@@ -1030,6 +1030,58 @@ namespace FLOOF {
             auto &script = m_Scene->AddComponent<NativeScriptComponent>(ent, std::make_unique<EnvironmentSoundScript>(), m_Scene.get(), ent);
         }
 
-    }
+        //make ramps
+        {
 
+            struct ramptform {
+                glm::vec3 pos{0.f};
+                glm::vec3 rot{0.f};
+                glm::vec3 scale{1.f};
+            };
+            std::vector<ramptform> ramps;
+
+            ramptform tf;
+            tf.pos = glm::vec3(-370.f,0.5f,133.f);
+            tf.rot= glm::vec3(-1.8f,0.f,0.f);
+            tf.scale = glm::vec3(0.1f,0.1f,0.06f);
+            ramps.emplace_back(tf);
+            //-370, 0.5, 133
+            //-1.8,0,0
+            //0.1, 0.1, 0.06
+
+            //135, -44, 200
+            //half pi, 2, 0
+            //0.2, 0.05, 0.05
+            tf.pos = glm::vec3(135.f,-44.f,200.f);
+            tf.rot= glm::vec3(-glm::pi<float>()/2.f,2.f,0.f);
+            tf.scale = glm::vec3(0.2f,0.05f,0.05f);
+            ramps.emplace_back(tf);
+            //56, -27, -279.1
+            //-1.6, 5.6, 0
+            //0.2
+            tf.pos = glm::vec3(56.f,-27.f,-279.1f);
+            tf.rot= glm::vec3(-1.6f,5.6f,0.f);
+            tf.scale = glm::vec3(0.2f);
+            ramps.emplace_back(tf);
+
+            for (auto ramp: ramps) {
+                auto extents = ramp.scale;
+                auto location = ramp.pos;
+                auto rotation = ramp.rot;
+                float mass = 0.f;
+                std::string name = "Ramp ";
+
+                auto entity = m_Scene->CreateEntity(name);
+                auto &collision = m_Scene->AddComponent<RigidBodyComponent>(entity, location, extents, rotation, mass, "Assets/ramp/scene.gltf");
+                auto &mesh = m_Scene->AddComponent<StaticMeshComponent>(entity, "Assets/ramp/scene.gltf");
+                auto &transform = m_Scene->GetComponent<TransformComponent>(entity);
+                transform.Position = glm::vec3(collision.Transform.getOrigin().getX(), collision.Transform.getOrigin().getY(), collision.Transform.getOrigin().getZ());
+                transform.Scale = extents;
+                transform.Rotation = rotation;
+                collision.RigidBody->setFriction(2.f);
+
+            }
+        }
+
+    }
 }
